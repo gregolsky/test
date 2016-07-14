ac38faf54d7baa5325ac1c4e12c8cfa410dcd525
Grzegorz Lachowski <gregory.lachowski@gmail.com>
2016-07-14 11:39:09 +0200

    Bump version to 3.5.1

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -10,9 +10,9 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(false)]
 [assembly: AssemblyTitle("RavenDB")]
-[assembly: AssemblyVersion("3.5.2")]
-[assembly: AssemblyFileVersion("3.5.2.13")]
-[assembly: AssemblyInformationalVersion("3.5.2")]
+[assembly: AssemblyVersion("3.5.1")]
+[assembly: AssemblyFileVersion("3.5.1.13")]
+[assembly: AssemblyInformationalVersion("3.5.1")]
 [assembly: AssemblyProduct("RavenDB")]
 [assembly: AssemblyDescription("RavenDB is a second generation LINQ enabled document database for .NET")]
 
